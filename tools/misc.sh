e9a20f8dbd48628182810acdb2f3042e2d410fc6
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-14 19:55:23 +0200

    Create misc.sh

---
+++ tools/misc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install dirsearch -y
+sudo apt install bloodyad -y
+sudo apt install seclists -y
+sudo apt install hoaxshell -y
+sudo apt install arjun -y
+sudo apt install sshpass -y
