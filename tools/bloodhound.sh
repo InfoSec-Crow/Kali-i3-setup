56471c4a5f53cd9bc1aa70d674d0eb8afcce6dd4
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-14 19:04:14 +0200

    Update bloodhound.sh

--- tools/bloodhound.sh
+++ tools/bloodhound.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-sudo apt install -y bloodhound
-sudo bloodhound-setup
+cd /opt
+wget https://github.com/BloodHoundAD/BloodHound/releases/latest/download/BloodHound-linux-x64.zip
+unzip BloodHound-linux-x64.zip
+rm -f BloodHound-linux-x64.zip
+
+sudo bash -c "cat > /usr/local/bin/bloodhound" <<EOF
+#!/bin/bash
+/opt/BloodHound-linux-x64/BloodHound --disable-gpu --disable-software-rasterizer --in-process-gpu "\$@"
+EOF
+
+sudo chmod +x /usr/local/bin/bloodhound
+
+mkdir -p /home/kali/Documents/Kali-i3-setup/.config/
 cat /home/kali/Documents/Kali-i3-setup/.config/customqueries.json > /home/kali/.config/bloodhound/customqueries.json
+
 sudo neo4j console &
 
 firefox http://localhost:7474
