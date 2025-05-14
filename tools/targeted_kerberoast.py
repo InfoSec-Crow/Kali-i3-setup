0d1f945989f65a8ab721720d7c36455a98a9cafd
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-14 19:51:31 +0200

    Create targeted_kerberoast.py

---
+++ tools/targeted_kerberoast.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt
+git clone https://github.com/ShutdownRepo/targetedKerberoast
+cd targetedKerberoast
+uv add --script targetedKerberoast.py -r requirements.txt
+sed -i '1s|.*|#!/usr/bin/env -S uv run --script|' targetedKerberoast.py
+sudo ln -s /opt/targetedKerberoast/targetedKerberoast.py /usr/local/bin/targetedKerberoast
