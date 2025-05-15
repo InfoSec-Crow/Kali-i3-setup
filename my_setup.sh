f3882ebd5f130325219b6293fc5ec09029b6a13c
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-15 13:39:42 +0200

    Update my_setup.sh

--- my_setup.sh
+++ my_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install
+## Go
+sudo apt install golang -y
+## uv 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+
 # Share Folder
 sudo mkdir /mnt/hgfs
 echo 'vmhgfs-fuse /mnt/hgfs fuse defaults,allow_other,_netdev   0   0' | sudo tee -a /etc/fstab
@@ -10,3 +17,7 @@ git clone https://github.com/InfoSec-Crow/CTF-Stuff
 sh /opt/CTF-Stuff/symlink.sh
 
 # Folders
+chown kali:kali /opt 
+chown kali:kali /etc/hosts
+
+
