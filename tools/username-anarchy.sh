2d31b00932681a5f181c4689e423b5274c35c882
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-15 11:20:24 +0200

    Update username-anarchy.sh

--- tools/username-anarchy.sh
+++ tools/username-anarchy.sh
@@ -3,3 +3,4 @@
 cd /opt
 git clone https://github.com/urbanadventurer/username-anarchy
 sudo ln -s /opt/username-anarchy/username-anarchy /usr/local/bin/username-anarchy
+chmod +x /opt/username-anarchy/username-anarchy
