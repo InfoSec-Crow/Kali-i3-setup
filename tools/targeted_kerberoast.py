1c2240150cdf854f752ff456cd8285c92a10cf78
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-15 11:13:18 +0200

    Update targeted_kerberoast.py

--- tools/targeted_kerberoast.py
+++ tools/targeted_kerberoast.py
@@ -5,4 +5,4 @@ git clone https://github.com/ShutdownRepo/targetedKerberoast
 cd targetedKerberoast
 uv add --script targetedKerberoast.py -r requirements.txt
 sed -i '1s|.*|#!/usr/bin/env -S uv run --script|' targetedKerberoast.py
-sudo ln -s /opt/targetedKerberoast/targetedKerberoast.py /usr/local/bin/targetedKerberoast
+sudo ln -s /opt/targetedKerberoast/targetedKerberoast.py /usr/local/bin/targetedKerberoast.py
