629e3a306f36f70a4f6e96c691487e54a6cb8e63
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-15 11:52:47 +0200

    Update compiled_binaries.sh

--- tools/compiled_binaries.sh
+++ tools/compiled_binaries.sh
@@ -5,10 +5,14 @@ cd /opt
 # Ghostpack-CompiledBinaries
 git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
 
+# SharpCollection
+git clone https://github.com/Flangvik/SharpCollection
+
 # GodPotato
 mkdir GodPotato
 wget -O GodPotato/GodPotato-NET4.exe $(curl -s https://api.github.com/repos/BeichenDream/GodPotato/releases/latest | grep "browser_download_url" | grep "GodPotato-NET4.exe" | cut -d '"' -f 4)
 wget -O GodPotato/GodPotato-NET35.exe $(curl -s https://api.github.com/repos/BeichenDream/GodPotato/releases/latest | grep "browser_download_url" | grep "GodPotato-NET35.exe" | cut -d '"' -f 4)
 wget -O GodPotato/GodPotato-NET2.exe $(curl -s https://api.github.com/repos/BeichenDream/GodPotato/releases/latest | grep "browser_download_url" | grep "GodPotato-NET2.exe" | cut -d '"' -f 4)
 
-#
+# nishang
+git clone https://github.com/samratashok/nishang
