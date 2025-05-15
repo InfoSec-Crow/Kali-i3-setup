dc7831fb5433f68da986cedb8eefb4bec8b745e2
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-15 11:52:53 +0200

    Update my_setup.sh

--- my_setup.sh
+++ my_setup.sh
@@ -4,4 +4,9 @@
 sudo mkdir /mnt/hgfs
 echo 'vmhgfs-fuse /mnt/hgfs fuse defaults,allow_other,_netdev   0   0' | sudo tee -a /etc/fstab
 
+# CTF Repo
+cd /opt
+git clone https://github.com/InfoSec-Crow/CTF-Stuff
+sh /opt/CTF-Stuff/symlink.sh
+
 # Folders
