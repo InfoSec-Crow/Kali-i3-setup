fdd6ebf75c7c11184fe4bfcd23216a536a05a351
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-17 19:41:34 +0200

    Update my_setup.sh

--- my_setup.sh
+++ my_setup.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-sudo apt update
-
-# Example Files
-mv example_files/ /home/kali/Documents/
-
-# install
-## Go
-sudo apt install golang -y
-## uv 
-curl -LsSf https://astral.sh/uv/install.sh | sh
+sh setup.sh
 
 # Share Folder
 sudo mkdir /mnt/hgfs
 sudo sh -c "echo 'vmhgfs-fuse /mnt/hgfs fuse defaults,allow_other,_netdev   0   0' >> /etc/fstab"
 
 # Files & Folders
-sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
-sudo chown kali:kali /opt 
-sudo chown kali:kali /etc/hosts
 sudo rm -r /home/kali/Music /home/kali/Templates /home/kali/Videos /home/kali/Public
-
-# CTF Repo
-git clone https://github.com/InfoSec-Crow/CTF-Stuff /opt/CTF-Stuff
-sh /opt/CTF-Stuff/symlink.sh
 
 # tmux
 mkdir -p ~/.tmux
@@ -34,6 +18,3 @@ git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sen
 git clone https://github.com/tmux-plugins/tmux-logging ~/.tmux/plugins/tmux-logging
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
 git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
-
-# locate error fix
-sudo updatedb
