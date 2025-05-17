bb73c93ef163badcf08ac240da59f961208fd0e6
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-17 19:40:34 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+
+# Example Files
+mv example_files/ /home/kali/Documents/
+
+# install
+## Go
+sudo apt install golang -y
+## uv 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Files & Folders
+sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+sudo chown kali:kali /opt 
+sudo chown kali:kali /etc/hosts
+
+# CTF Repo
+git clone https://github.com/InfoSec-Crow/CTF-Stuff /opt/CTF-Stuff
+sh /opt/CTF-Stuff/symlink.sh
