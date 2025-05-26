a56427c9dc01ecf4c5934178bc6d7933252fa5b9
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-26 09:27:14 +0200

    Update krbrelayx.sh

--- tools/krbrelayx.sh
+++ tools/krbrelayx.sh
@@ -3,7 +3,7 @@
 cd /opt
 git clone https://github.com/dirkjanm/krbrelayx
 
-sudo ln -s /opt/krbrelayx/addspn.py.py /usr/local/bin/addspn.py.py
+sudo ln -s /opt/krbrelayx/addspn.py /usr/local/bin/addspn.py
 sudo ln -s /opt/krbrelayx/dnstool.py /usr/local/bin/dnstool.py
 sudo ln -s /opt/krbrelayx/krbrelayx.py /usr/local/bin/krbrelayx.py
 sudo ln -s /opt/krbrelayx/printerbug.py /usr/local/bin/printerbug.py
